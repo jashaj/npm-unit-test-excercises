df6c3c546618c8f7ec5451bbedf98bddd4621e25
Jasha Joachimsthal <jashaj@gmail.com>
2017-03-08 08:28:30 +0100

    Test an inner function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 "use strict";
+
+function calcBmi(weight, height) {
+  return weight / (Math.pow(height, 2));
+}
+
 module.exports = {
-  "add": function add(a, b) {
-    return a + b;
+  "add": (a, b) => a + b,
+  "bmi": (weight, height) => {
+    let bmi = calcBmi(weight, height).toFixed(1);
+    return `${weight} / (${height}^2) = ${bmi}`;
   }
 };
--- test/add-spec.js
+++ test/add-spec.js
@@ -1,13 +1,16 @@
 "use strict";
+const rewire = require('rewire');
 const assert = require('assert');
+
 describe('module', () => {
   let module;
   beforeEach(() => {
-    module = require('../index');
+    module = rewire('../index');
   });
-  describe('#add()', function () {
+
+  describe('#add()', () => {
     it('is a function', () => {
-      console.assert(typeof module.add === 'function');
+      assert.strictEqual('function', typeof module.add);
     });
     it('should add numbers', () => {
       assert.equal(3, module.add(1, 2));
@@ -16,4 +19,25 @@ describe('module', () => {
       assert.equal('foobar', module.add('foo', 'bar'));
     })
   });
+
+  describe('#bmi()', function () {
+    const weight = 68;
+    const height = 1.65;
+
+    it('is a function', () => {
+      assert.strictEqual('function', typeof module.bmi);
+    });
+    it('returns a string', () => {
+      assert.strictEqual('string', typeof module.bmi(weight, height));
+    });
+    it('calculates the BMI with 1 digit', () => {
+      assert.equal('68 / (1.65^2) = 25.0', module.bmi(weight, height));
+    });
+    it('has inner function to calculate the BMI', () => {
+      assert('undefined', typeof module.calcBmi);
+
+      let calcBmi = module.__get__('calcBmi');
+      assert.equal(25, Math.round(calcBmi(weight, height)));
+    });
+  })
 });
